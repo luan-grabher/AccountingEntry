29eb61eca932533b35f1e63b72934bf348b64ad8
Luan Grabher <lujetix@hotmail.com>
2020-11-09 09:50:18 -0300

    Fix 'AND' in sql file 'selectContabilityentries.sql'

--- sql/selectContabilityEntries.sql
+++ sql/selectContabilityEntries.sql
@@ -4,3 +4,5 @@ FROM VSUC_EMPRESAS_TLAN L
 WHERE
 BDCODEMP = :enterprise
 AND BDDATA >= ':dateStart' AND BDDATA <= ':dateEnd'
+AND (l.BDCREDITO = :account OR L.BDDEBITO = :account)
+AND (:participant is NULL OR (:participant is not NULL AND (BDCODTERCEIROC = :participant OR BDCODTERCEIROD = :participant)))
