e584e1306eca6644dba0141a1a3ca4904051142f
Admnistrador <Admnistrador@192.168.0.31>
2020-12-28 17:38:16 -0300

    fixed conciled value to sql in get entries/count before date

--- sql/getEntriesCountBeforeDate.sql
+++ sql/getEntriesCountBeforeDate.sql
@@ -5,5 +5,5 @@ WHERE
 BDCODEMP = :enterprise
 AND BDDATA < ':date'
 AND (BDDEBITO = :account OR BDCREDITO = :account)
-AND CAST(L.BDCONCC AS VARCHAR(10)) :conciled 'TRUE' AND CAST(L.BDCONCD AS VARCHAR(10)) :conciled 'TRUE'
+AND CAST(BDCONCC AS VARCHAR(10)) :conciled 'TRUE' AND CAST(BDCONCD AS VARCHAR(10)) :conciled 'TRUE'
 AND (:participant is NULL OR (:participant is not NULL AND (BDCODTERCEIROC = :participant OR BDCODTERCEIROD = :participant)))
