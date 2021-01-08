4e3e01a388b60f597d448c2549fb016540d05898
Admnistrador <Admnistrador@192.168.0.31>
2021-01-08 10:34:45 -0300

    Added throw error in update entries on database

--- src/lctocontabil/Model/ContabilityEntries_Model.java
+++ src/lctocontabil/Model/ContabilityEntries_Model.java
@@ -5,6 +5,7 @@ import SimpleView.Loading;
 import fileManager.FileManager;
 import java.math.BigDecimal;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
@@ -109,51 +110,39 @@ public class ContabilityEntries_Model {
      * informado
      *
      * @param entries Mapa de lançamentos
-     * @return Retorna true se ocorrer tudo Ok
+     * @throws java.sql.SQLException Se ocorrer um erro causa uma exceção
      */
-    public static boolean updateContabilityEntriesOnDatabase(Map<Integer, ContabilityEntry> entries) {
-        try {
+    public static void updateContabilityEntriesOnDatabase(Map<Integer, ContabilityEntry> entries) throws SQLException {
+        Loading loading = new Loading("Atualizando lançamentos do banco", 0, entries.size());
+        int count = 0;
 
-            Loading loading = new Loading("Atualizando lançamentos do banco", 0, entries.size());
-            int count = 0;
+        for (Map.Entry<Integer, ContabilityEntry> entry : entries.entrySet()) {
+            //Atualiza barra de carregamento
+            count++;
+            loading.updateBar(count + " de " + entries.size(), count);
 
-            for (Map.Entry<Integer, ContabilityEntry> entry : entries.entrySet()) {
-                //Atualiza barra de carregamento
-                count++;
-                loading.updateBar(count + " de " + entries.size(), count);
+            Integer key = entry.getKey();
+            ContabilityEntry e = entry.getValue();
 
-                Integer key = entry.getKey();
-                ContabilityEntry e = entry.getValue();
+            Map<String, String> swaps = new TreeMap<>();
+            swaps.put("key", e.getKey().toString());
+            swaps.put("enterprise", e.getEnterprise().toString());
+            swaps.put("date", Dates.getCalendarInThisStringFormat(e.getDate(), "YYYY-MM-dd"));
+            swaps.put("defaultPlan", e.getDefaultPlan().toString());
+            swaps.put("accountDebit", e.getAccountDebit() == 0 ? "NULL" : e.getAccountDebit().toString());
+            swaps.put("accountCredit", e.getAccountCredit() == 0 ? "NULL" : e.getAccountCredit().toString());
+            swaps.put("participantDebit", e.getParticipantDebit() == 0 ? "NULL" : e.getParticipantDebit().toString());
+            swaps.put("participantCredit", e.getParticipantCredit() == 0 ? "NULL" : e.getParticipantCredit().toString());
+            swaps.put("historyCode", e.getHistoryCode().toString());
+            swaps.put("complement", e.getComplement());
+            swaps.put("document", e.getDocument());
+            swaps.put("value", e.getValue().toPlainString());
+            swaps.put("conciliedCredit", "'" + e.getConciliedCredit().toString().toUpperCase() + "'");
+            swaps.put("conciliedDebit", "'" + e.getConciliedDebit().toString().toUpperCase() + "'");
 
-                Map<String, String> swaps = new TreeMap<>();
-                swaps.put("key", e.getKey().toString());
-                swaps.put("enterprise", e.getEnterprise().toString());
-                swaps.put("date", Dates.getCalendarInThisStringFormat(e.getDate(), "YYYY-MM-dd"));
-                swaps.put("defaultPlan", e.getDefaultPlan().toString());
-                swaps.put("accountDebit", e.getAccountDebit() == 0 ? "NULL" : e.getAccountDebit().toString());
-                swaps.put("accountCredit", e.getAccountCredit() == 0 ? "NULL" : e.getAccountCredit().toString());
-                swaps.put("participantDebit",e.getParticipantDebit() == 0 ? "NULL" : e.getParticipantDebit().toString());
-                swaps.put("participantCredit",e.getParticipantCredit() == 0 ? "NULL" : e.getParticipantCredit().toString());
-                swaps.put("historyCode", e.getHistoryCode().toString());
-                swaps.put("complement", e.getComplement());
-                swaps.put("document", e.getDocument());
-                swaps.put("value", e.getValue().toPlainString());
-                swaps.put("conciliedCredit", "'" + e.getConciliedCredit().toString().toUpperCase() + "'");
-                swaps.put("conciliedDebit", "'" + e.getConciliedDebit().toString().toUpperCase() + "'");
-
-                //Se ocorrer algum erro ao fazer o update, para o código
-                if (!db.query(sql_updateContabilityEntriesOnDatabase, swaps)) {
-                    return false;
-                }
-            }
-
-            return true;
-        } catch (Exception e) {
-            System.out.println("[ContabilityEntries_Model] Erro ao fazer update no banco: " + e);
-            e.printStackTrace();
+            //Se ocorrer algum erro ao fazer o update, para o código e causa um erro
+            db.query(sql_updateContabilityEntriesOnDatabase, swaps);
         }
-
-        return false;
     }
 
     /**
@@ -181,10 +170,9 @@ public class ContabilityEntries_Model {
      * @param keys Lista de chaves que serão conciliadas em formato de texto
      * separadas por ,
      * @param concilited TRUE para conciliar e FALSE para desconciliar
-     * @return Retorna TRUE se conseguir executar a query e FALSE se não
-     * conseguir
+     * @throws java.sql.SQLException Causa um erro se algo de errado acontecer
      */
-    public static boolean conciliateKeysOnDatabase(Integer enterprise, String keys, Boolean concilited) {
+    public static void conciliateKeysOnDatabase(Integer enterprise, String keys, Boolean concilited) throws SQLException {
         String whereIn = SQL.divideIn(keys, "BDCHAVE");
 
         Map<String, String> swaps = new HashMap<>();
@@ -193,7 +181,7 @@ public class ContabilityEntries_Model {
         swaps.put("concilited", "'" + concilited.toString().toUpperCase() + "'");
 
         /*Se conseguir fazer a query retorna TRUE*/
-        return db.query(sql_conciliateKeys, swaps);
+        db.query(sql_conciliateKeys, swaps);
     }
 
     /**
@@ -250,18 +238,18 @@ public class ContabilityEntries_Model {
      * @param date Data limite que busca os lançamentos
      * @param enterprise número da empresa no único
      * @param account Conta contábil
-     * @param conciled  Se está conciliado ou não, deixe nulo para filtrar todos
+     * @param conciled Se está conciliado ou não, deixe nulo para filtrar todos
      * @param participant Número do participante, se não tiver deve ficar nulo
      *
      * @return Retorna o número de lançamentos anteriores a data
      */
-    public static Long getEntriesCountBeforeDate(Calendar date, Integer enterprise, Integer account,Boolean conciled, Integer participant) {
+    public static Long getEntriesCountBeforeDate(Calendar date, Integer enterprise, Integer account, Boolean conciled, Integer participant) {
         /*Cria trocas*/
         Map<String, String> swaps = new TreeMap<>();
         swaps.put("enterprise", enterprise.toString());
         swaps.put("date", Dates.getCalendarInThisStringFormat(date, "YYYY-MM-dd"));
         swaps.put("account", account.toString());
-        swaps.put("conciled", conciled.equals(Boolean.TRUE)?"=":"<>");
+        swaps.put("conciled", conciled.equals(Boolean.TRUE) ? "=" : "<>");
         swaps.put("participant", participant == null ? "NULL" : participant.toString());
 
         List<Map<String, Object>> results = db.getMap(sql_getEntriesCountBeforeDate, swaps);
@@ -275,18 +263,18 @@ public class ContabilityEntries_Model {
      * @param date Data limite que busca os lançamentos
      * @param enterprise número da empresa no único
      * @param account Conta contábil
-     * @param conciled  Se está conciliado ou não, deixe nulo para filtrar todos
+     * @param conciled Se está conciliado ou não, deixe nulo para filtrar todos
      * @param participant Número do participante, se não tiver deve ficar nulo
      *
      * @return Retorna lista de lançamentos anteriores a data
      */
-    public static String getEntriesListBeforeDate(Calendar date, Integer enterprise, Integer account,Boolean conciled, Integer participant) {
+    public static String getEntriesListBeforeDate(Calendar date, Integer enterprise, Integer account, Boolean conciled, Integer participant) {
         /*Cria trocas*/
         Map<String, String> swaps = new TreeMap<>();
         swaps.put("enterprise", enterprise.toString());
         swaps.put("date", Dates.getCalendarInThisStringFormat(date, "YYYY-MM-dd"));
         swaps.put("account", account.toString());
-        swaps.put("conciled", conciled.equals(Boolean.TRUE)?"=":"<>");
+        swaps.put("conciled", conciled.equals(Boolean.TRUE) ? "=" : "<>");
         swaps.put("participant", participant == null ? "NULL" : participant.toString());
 
         List<Map<String, Object>> results = db.getMap(sql_selectEntriesListBeforeDate, swaps);
